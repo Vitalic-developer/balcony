47b0ebf5d68a6e0099ca7be1017e239459650c2e
Vitalic-developer <raven19931125@gmail.com>
2024-11-30 08:28:11 +0300

    add popup-2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-let modal = document.getElementById('myModal');
+document.addEventListener('DOMContentLoaded', function() {
+    let modal = document.getElementById('myModal');
+    let btn = document.getElementById("signUp");
+    let close = document.getElementsByClassName("close")[0];
 
-let btn = document.getElementById("signUp");
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-let span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-    if (event.target == modal) {
+    close.onclick = function() {
         modal.style.display = "none";
     }
-}
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    let modalGift = document.getElementById('myModalGift');
+    let btnDetails = document.getElementById("btnDetails");
+    let closeGift = document.getElementsByClassName("close")[1];
+
+    btnDetails.onclick = function() {
+        modalGift.style.display = "block";
+    }
+
+    closeGift.onclick = function() {
+        modalGift.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target === modalGift) {
+            modalGift.style.display = "none";
+        }
+    }
+});
