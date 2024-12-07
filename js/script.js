c21a891f0a732fcc42a907ecde1b29f4e213a858
Vitalic-developer <raven19931125@gmail.com>
2024-12-07 22:27:59 +0300

    refactor code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let modal = document.getElementById('myModal');
     let btn = document.getElementById("signUp");
     let close = document.getElementsByClassName("close")[0];
 
-    btn.onclick = function() {
+    btn.onclick = function () {
         modal.style.display = "block";
     }
 
-    close.onclick = function() {
+    close.onclick = function () {
         modal.style.display = "none";
     }
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (event.target === modal) {
             modal.style.display = "none";
         }
@@ -21,17 +21,30 @@ document.addEventListener('DOMContentLoaded', function() {
     let btnDetails = document.getElementById("btnDetails");
     let closeGift = document.getElementsByClassName("close")[1];
 
-    btnDetails.onclick = function() {
+    btnDetails.onclick = function () {
         modalGift.style.display = "block";
     }
 
-    closeGift.onclick = function() {
+    closeGift.onclick = function () {
         modalGift.style.display = "none";
     }
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (event.target === modalGift) {
             modalGift.style.display = "none";
         }
     }
+});
+
+//slider
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: '5', // Количество слайдов на экране
+        spaceBetween: 20, // Расстояние между слайдами
+        loop: true, // Бесконечный цикл
+        autoplay: {
+            delay: 3500, // Автопрокрутка с задержкой в 2.5 секунды
+            disableOnInteraction: false, // Не останавливать автопрокрутку при взаимодействии
+        },
+    });
 });
