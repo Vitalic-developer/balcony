f3328f2a4c49dfd4acccd6702b539b304bad5e44
Vitalik <raven19931125@gmail.com>
2025-06-09 18:59:31 +0300

    first commit

---
+++ js/form.js
@@ -0,0 +1,32 @@
+const forms = document.forms;
+
+for (let i = 0; i < forms.length; i++) {
+    const form = forms[i];
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const name = e.target.querySelector("[name='name']").value;
+        const phone = e.target.querySelector("[name='phone']").value;
+        const type = e.target.querySelector("[name='type']")?.value;
+
+        try {
+            const response = await fetch("https://server.oknobalkon.by/sendForm", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({name: name, phone: phone, type: type})
+            });
+            // из объекта ответа извлекаем текст ответа
+            const responseText = await response.text();
+            const modalThanks = document.getElementById('modalThanks');
+            modalThanks.className += ' active';
+
+            setTimeout(() => { modalThanks.className = 'modal-thanks'; }, 5000);
+
+        } catch (err){
+            alert('Произошла ошибка, попробуйте позже');
+
+            console.log('err', err);
+        }
+    })
+}
+
